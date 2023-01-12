571638718110244dd8d5a9a2ccada06e920d73f2
catlehoang <56531547+catlehoang@users.noreply.github.com>
2023-01-13 01:07:58 +0700

    add grid view for order

---
+++ src/pages/Order/Edit/GridView.tsx
@@ -0,0 +1,50 @@
+import { Button, DataTable, Badge } from '@shopify/polaris';
+import { useMemo } from 'react';
+
+type Props = {
+	data?: any[];
+	//onDelete: (i: number) => void;
+};
+
+interface Column {
+	type: 'text' | 'numeric';
+	heading: React.ReactNode;
+	sortable?: boolean;
+	field: string;
+}
+
+const columns: Column[] = [
+	{ type: 'text', heading: 'Client name', field: 'name' },
+	{ type: 'text', heading: 'Date ordered', field: 'date' }, //change type to date
+	{ type: 'text', heading: 'Status', field: 'status' },
+    { type: 'text', heading: 'Action', field: 'action' },
+];
+
+const GridView = ({ data}: Props) => {
+	const rows = useMemo(
+		() =>
+			(data || []).map((item, index) =>
+				columns.map((column) => {
+					if (column.field === 'status') {
+						return <Badge status={item.status === 'available' ? 'success' : 'warning'}>{item.status}</Badge>
+					}
+					return item[column.field as keyof typeof item] as string;
+				})
+			),
+		[data]
+	);
+
+	return (
+		<div className="mb-3">
+			<div className="relative">
+				<DataTable
+					columnContentTypes={columns.map((item) => item.type)}
+					headings={columns.map((item) => item.heading)}
+					rows={rows}
+					sortable={columns.map((item) => item.sortable || false)}
+				/>
+			</div>
+		</div>
+	);
+};
+export default GridView;
